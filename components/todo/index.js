3027c3f4d85d4ae54c43bb6ca5523f06985427ab
tommycoleman87 <tommiilee@gmail.com>
2019-07-09 19:19:37 -0400

    finished mvp and stretch

--- components/todo/index.js
+++ components/todo/index.js
@@ -3,13 +3,13 @@ class TodoComponent {
         // Assign outer TodoComponent Element. We should do all of our searching within here, not `document`.
         this.componentElement = componentElement;
         // Get the todos container element
-        this.todosElement;
+        this.todosElement = this.componentElement.querySelector('.todos-container');
         // instantiate the Todos class with it
-        this.todos;
+        this.todos = new Todos(this.todosElement);
         // Do the same with form Element
-        this.formElement;
+        this.formElement = this.componentElement.querySelector('.todo-form');
         // I've given you a hint here. Look at the TodoForm constructor.
-        this.form; 
+        this.form = new TodoForm(this.formElement, this.todos)
     }
 }
 
@@ -19,16 +19,25 @@ class Todos {
     }
     addTodo(text) {
         // Add a todo element to the container, and instantiate its class
+        console.log(text)
+        let todoElement = document.createElement('div');
+        todoElement.className = 'todo';
+        todoElement.textContent = text;
+        this.containerElement.appendChild(todoElement);
+        todoElement = new Todo(todoElement);
+        
     }
 }
 
 class Todo {
     constructor(todoElement) {
         this.todoElement = todoElement;
+        this.todoElement.addEventListener('click',() => this.toggle())
         // What do we need to add to make our element to make `this.toggle` work?
     }
     toggle() {
         // Toggle the element being 'done'
+        this.todoElement.classList.toggle('done');
     }
 }
 
@@ -39,18 +48,36 @@ class TodoForm {
         this.todos = todos;
         this.input = this.formElement.querySelector('input');
         this.addButton = this.formElement.querySelector('.add');
+        this.addButton.addEventListener('click', () => this.submitTodo())
 
         // stretch - make a button clear all completed todos
-        // this.clearButton;
+        this.clearButton = this.formElement.querySelector('.clear');
+        this.clearButton.addEventListener('click', () => this.clearTodo());
 
         // https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
     }
+
+    clearTodo() {
+        event.preventDefault();
+        const todos = document.querySelectorAll('.todo');
+        console.log(todos)
+        todos.forEach(todo => {
+            if(todo.classList.contains('done')){
+                todo.classList.add('hide');
+            }
+        })
+    }
     submitTodo() {
+        event.preventDefault();
         // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
         // see 'value'. 
 
         // We need to actually add a todo to the page. If only we had access to
         // a class that has a member function that does just that.
+        console.log(this.input.value)
+        this.todos.addTodo(this.input.value);
+        
+
     }
 }
 
